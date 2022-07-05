61972480cf2e525e007b9bd39188a65255398697
Nayzawthant <108381970+Nayzawthant@users.noreply.github.com>
2022-07-05 03:06:03 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu-btn')
-const menuSecond = document.querySelector('.menuSecond')
-
-menuBtn.addEventListener('click',()=>{
-    menuSecond.classList.toggle('mobile-menu')
-});
